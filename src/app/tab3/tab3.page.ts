b566c3312dc517c97c774d7e5d598b788cdcf923
chlsedu <cdgedu@qq.com>
2020-04-16 18:14:23 +0800

    1

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CommonService} from '../services/common.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+  hideHeaderObj = {hideHeader: false};
+
+  constructor(public commonService: CommonService) {
+  }
+
+  ngOnInit() {
+  }
 
 }
